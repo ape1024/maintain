882aa2071ceb0ac39d47773940eda727cd19ba66
zhaoxi521 <646151275@qq.com>
2018-08-06 16:36:21 +0800

    增加一个图片轮播的弹窗

---
+++ src/base/dialog-img/dialog-img.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="dialog-img" @click="close" v-show="state">
+    <div class="wrapper" @click.stop>
+      <el-carousel ref="carousel" :autoplay="false" trigger="click" class="carousel">
+        <el-carousel-item v-for="(item, index) in list" :key="index">
+          <div class="item">
+            <img :src="item">
+          </div>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      state: false
+    }
+  },
+  methods: {
+    close () {
+      this.state = false
+    },
+    open () {
+      this.state = true
+    },
+    switchIndex (index) {
+      if (index > (this.list.length - 1)) return
+      this.$refs.carousel.setActiveItem(index)
+    }
+  }
+}
+</script>
+
+<style scoped rel="stylesheet/stylus" lang="stylus">
+  .dialog-img
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    z-index 1000
+    background rgba(0, 0, 0, .6)
+    .wrapper
+      width 600px
+      height 300px
+      margin 15% auto 0
+      .carousel
+        width 100%
+        height 100%
+        .item
+          width 100%
+          height 100%
+          img
+            display block
+            height 100%
+            width auto
+            margin 0 auto
+</style>
