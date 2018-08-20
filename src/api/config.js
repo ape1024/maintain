8db311c9e61a2c6f6880447e13bbc7c5fb73884a
qq799216818 <www.youyouhao@qq.com>
2018-08-20 15:13:58 +0800

    2018.8.20

--- src/api/config.js
+++ src/api/config.js
@@ -5,8 +5,8 @@ const projectOsmUrl = 'http://172.16.5.34:8080'
 export const osmUrl = debug ? debugOsmUrl : projectOsmUrl
 
 // const debugUrl = 'http://172.16.6.99:8910'
-const debugUrl = 'http://172.16.5.35:18920'
-const projectUrl = 'http://172.16.5.35:18920'
+const debugUrl = window.Test
+const projectUrl = window.Test
 export const URL = debug ? debugUrl : projectUrl
 
 export const USER_LOGIN_OK = 0
