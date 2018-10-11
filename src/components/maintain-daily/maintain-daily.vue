9d649c169f4d56b9e18dec1f8331dec8ace3ae92
qq799216818 <www.youyouhao@qq.com>
2018-10-11 14:10:00 +0800

    updata

--- src/components/maintain-daily/maintain-daily.vue
+++ src/components/maintain-daily/maintain-daily.vue
@@ -176,7 +176,7 @@ export default {
   },
   methods: {
     pigeonhole (taskID) {
-      this.$confirm('是否将选中的多项工作记录批量审核为归档?', '提示', {
+      this.$confirm('是否归档?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
--- src/components/maintain-inspect/maintain-inspect.vue
+++ src/components/maintain-inspect/maintain-inspect.vue
@@ -174,7 +174,7 @@ export default {
   },
   methods: {
     pigeonhole (taskID) {
-      this.$confirm('此操作将归档此任务,归档后将不能查看 是否继续?', '提示', {
+      this.$confirm('是否归档?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
