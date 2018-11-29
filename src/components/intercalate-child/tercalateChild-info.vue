e00bd182ac3327f9b39972c28b0c509d838f0e6c
qq799216818 <www.youyouhao@qq.com>
2018-11-29 11:29:50 +0800

    updata

--- src/components/intercalate-child/tercalateChild-info.vue
+++ src/components/intercalate-child/tercalateChild-info.vue
@@ -164,15 +164,6 @@ export default {
     }
   },
   created () {
-    // const promise = new Promise({resolve, reject} => {
-    //   console.log(1)
-    //   resolve()
-    //   console.log(2)
-    // })
-    // promise.then(()=>{
-    //   console.log(3)
-    // })
-    // console.log(4)
     let Jurisdiction = JSON.parse(window.sessionStorage.Jurisdiction)
     Jurisdiction.forEach((val) => {
       if (val.functioncode === 'info') {
